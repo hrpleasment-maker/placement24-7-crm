2d90ef31440a93832af0f611aee24f098d7190d0
hrpleasment-maker <hrpleasment@gmail.com>
2026-07-27 12:35:12 +0530

    Update supabase.ts

--- api/supabase.ts
+++ api/supabase.ts
@@ -1,6 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.VITE_SUPABASE_URL!;
+const supabaseServiceKey =
+  process.env.SUPABASE_SERVICE_ROLE_KEY ||
+  process.env.VITE_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(
-  process.env.VITE_SUPABASE_URL!,
-  process.env.VITE_SUPABASE_ANON_KEY!
+  supabaseUrl,
+  supabaseServiceKey
 );
